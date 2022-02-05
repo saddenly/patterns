8957be57e2f70657e2ea42bb7bb42d6ea3043f7e
saddenly <rustemandasov6@gmail.com>
2022-02-05 15:04:20 +0600

    behavioral: iterator

---
+++ src/behavioral/iterator/spammer/SocialSpammer.java
@@ -0,0 +1,27 @@
+package iterator.spammer;
+
+import iterator.iterators.ProfileIterator;
+import iterator.profile.Profile;
+import iterator.social_networks.SocialNetwork;
+
+public class SocialSpammer {
+    public SocialNetwork network;
+    public ProfileIterator iterator;
+
+    public SocialSpammer(SocialNetwork network) {
+        this.network = network;
+    }
+
+    public void sendSpamToFriends(String profileEmail, String message) {
+        System.out.println("Iterating over friends...\n");
+        iterator = network.createFriendsIterator(profileEmail);
+        while (iterator.hasNext()) {
+            Profile profile = iterator.getNext();
+            sendMessage(profile.getEmail(), message);
+        }
+    }
+
+    public void sendMessage(String email, String message) {
+        System.out.println("Sent message to: '" + email + "'. Message body: '" + message + "'");
+    }
+}
